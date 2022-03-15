51abe075697c72b2b615277b393fcea2fa8a4bfb
fred <fredwright95@yahoo.com>
2022-03-15 15:03:35 -0400

    created function to update specific product

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,13 +19,20 @@ def products_list(request):
           return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)    
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+      serializer = ProductSerializer(product);
+      return Response(serializer.data)
+    elif request.method == 'PUT':
+         serializer = ProductSerializer(product, data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data)
+
+
+     
+           
 
                
