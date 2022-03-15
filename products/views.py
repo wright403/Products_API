8c2f5cd64da522f6bea241885f1da32c11f7d889
fred <fredwright95@yahoo.com>
2022-03-15 15:15:07 -0400

    created delete function

--- products/views.py
+++ products/views.py
@@ -19,17 +19,20 @@ def products_list(request):
           return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
-      serializer = ProductSerializer(product);
+      serializer = ProductSerializer(product)
       return Response(serializer.data)
     elif request.method == 'PUT':
          serializer = ProductSerializer(product, data=request.data)
          serializer.is_valid(raise_exception=True)
          serializer.save()
          return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)     
 
 
      
