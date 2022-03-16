d038201bfcbed7decc9a3d0b393b1935a172ec76
fred <fredwright95@yahoo.com>
2022-03-16 11:49:28 -0400

    added image to our models class and migrated over to the database.

--- products/models.py
+++ products/models.py
@@ -6,3 +6,5 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.CharField(max_length=255)
+
--- products/serializers.py
+++ products/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'price', 'inventory_quantity']
+        fields = ['id', 'title', 'description', 'price', 'inventory_quantity', 'image',]
